85c0f5267c15bfea99b6dbba245f7ffb7322941d
Jacob Gossett <jgossett121@gmail.com>
2016-03-08 01:10:41 -0600

    basic framework

--- script.js
+++ script.js
@@ -3,22 +3,49 @@ var Vehicle = function(){};
 Vehicle.prototype.move = function(){
     console.log('moved');
 }
-Vehicle.prototype.damage = function(){}
+Vehicle.prototype.damage = function(){
+    console.log('bang');
+}
 Vehicle.prototype.total = function(){}
 
 function Car() {
     Vehicle.call(this);
 }
 
-Car.prototype = Object.create(Vehicle.prototype);
-Car.prototype.constructer = Car;
-
-Car.prototype.move = function() {
-    console.log('no');
+function CopCar () {
+    Car.call(this);
 }
 
+function Motorcycle () {
+    Vehicle.call(this);
+}
+
+function Tank () {
+    Vehicle.call(this);
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructer = Car;
+CopCar.prototype = Object.create(Car.prototype);
+CopCar.prototype.constructer = CopCar;
+Motorcycle.prototype = Object.create(Vehicle.prototype);
+Motorcycle.prototype.constructer = Motorcycle;
+Tank.prototype = Object.create(Vehicle.prototype);
+Tank.prototype.constructer = Tank;
+
+
 var car = new Car();
-car.move();
+
+
+var copcar = new CopCar();
+
+var moto = new Motorcycle();
+moto.move();
+
+var tank = new Tank();
+tank.damage();
+
+
 
 
 
